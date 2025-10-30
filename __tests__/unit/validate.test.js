d2c1996772576ad4a0ed849ee99afb4e2cc68e17
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-30 19:31:02 +0900

    feat(validate): 문자로 입력된 금액 숫자로 변환
    - 아래 함수들 추가
      - convertToNumber
      - breakIntoArr
      - translate
    - 유닛 테스트

--- __tests__/unit/validate.test.js
+++ __tests__/unit/validate.test.js
@@ -1,4 +1,10 @@
-import { checkIfForbiden, clearInput } from "../../src/validate";
+import {
+  breakIntoArr,
+  checkIfForbidden,
+  clearInput,
+  convertToNumber,
+  translate,
+} from "../../src/validate";
 
 describe("clearInput", () => {
   test.each([
@@ -7,16 +13,52 @@ describe("clearInput", () => {
     ["14 000", "14000"],
     ["14 000 원", "14000"],
   ])("정상: %s", (input, cleaned) => {
-    const result = clearInput("ammount", input);
+    const result = clearInput("amount", input);
     expect(result).toBe(cleaned);
   });
 });
 
 describe("checkIfForbiden", () => {
   test.each(["*$원", "14억원", "가나다"])("에러 : %s", (input) => {
-    expect(() => checkIfForbiden(input)).toThrow(
+    expect(() => checkIfForbidden(input)).toThrow(
       `[ERROR] 올바르지 않은 입력입니다.`
     );
   });
-  test;
+});
+
+describe("convertToNumber", () => {
+  test.each([
+    ["3만", 30000],
+    ["삼만", 30000],
+    ["30000", 30000],
+  ])("%s", (str, number) => {
+    const result = convertToNumber(str);
+    expect(result).toBe(number);
+  });
+});
+
+describe("breakIntoArr", () => {
+  test.each([
+    ["30만", ["30", ""]],
+    ["3십만", ["3십", ""]],
+    ["삼십만", ["삼십", ""]],
+    ["삼십사만육천칠백팔십구", ["삼십사", "육천칠백팔십구"]],
+    ["34만6천8백", ["34", "6천8백"]],
+    ["5천", ["", "5천"]],
+  ])("%s", (str, arr) => {
+    const result = breakIntoArr(str);
+    expect(result).toEqual(arr);
+  });
+});
+
+describe("translate", () => {
+  test.each([
+    ["육천칠백팔십구", 6789],
+    ["육천팔십", 6080],
+    ["육천구", 6009],
+    ["6천7백8십9", 6789],
+  ])("%s", (str, a) => {
+    const result = translate(str);
+    expect(result).toBe(a);
+  });
 });
--- src/validate.js
+++ src/validate.js
@@ -1,22 +1,40 @@
-import { FORBIDDEN_INPUT, MAXIMUM_AMMOUNT, MINIMUM_AMMOUNT } from "./constant";
+import {
+  FORBIDDEN_INPUT,
+  MAXIMUM_AMMOUNT,
+  MINIMUM_AMMOUNT,
+  NUMBER_MAP,
+  DIGIT_NUMBER_MAP,
+} from "./constant";
 
 // 입력 금액 검증
 export const clearInput = (type, input) => {
-  if (type === "ammount") return input.replaceAll(/[ ,원]/g, "");
+  if (type === "amount") return input.replaceAll(/[ ,원]/g, "");
   if (type === "number") return input.replaceAll(" ", "");
 };
 
-export const checkIfForbiden = (input) => {
+export const checkIfForbidden = (input) => {
   if (FORBIDDEN_INPUT.test(input)) {
     throw new Error(`[ERROR] 올바르지 않은 입력입니다.`);
   }
 };
 
-export const convertToNumber = (valid) => {
-  if (Number(valid)) return valid;
+export const convertToNumber = (cleaned) => {
+  // "25000" -> 25000
+  if (Number(cleaned)) return Number(cleaned);
+
+  // "2만5천", "이만오천" -> ["2","5천"]
+  const arr = breakIntoArr(cleaned);
+
+  // ["2","5천"] -> [2,5000]
+  const [man, il] = arr.map(translate);
+
+  //[2,5000] -> 25000
+  let result = man * 10000 + il;
+
+  return result;
 };
 
-export const checkAmmountRange = (ammount) => {
+export const checkAmountRange = (ammount) => {
   if (ammount > MAXIMUM_AMMOUNT) {
     throw new Error(`[ERROR] 1회 구매 한도는 10만원 100장 입니다.`);
   }
@@ -26,4 +44,25 @@ export const checkAmmountRange = (ammount) => {
   if (ammount < MINIMUM_AMMOUNT) {
     throw new Error(`[ERROR] 최소 1,000원 이상의 금액을 입력해주세요.`);
   }
+};
+
+export const breakIntoArr = (cleaned) => {
+  if (!cleaned.includes("만")) return ["", cleaned];
+  return cleaned.split("만");
+};
+
+export const translate = (str) => {
+  // "오천" -> ["","오천"] -> [0,5000]
+  if (str === "") return 0;
+
+  let result = [0, 0, 0, 0];
+  let digitIndex = 3;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const ch = str[i];
+    if (ch in DIGIT_NUMBER_MAP) digitIndex = DIGIT_NUMBER_MAP[ch];
+    if (ch in NUMBER_MAP) result[digitIndex] = NUMBER_MAP[ch];
+    if (Number(ch)) result[digitIndex] = Number(ch);
+  }
+  return Number(result.join(""));
 };
