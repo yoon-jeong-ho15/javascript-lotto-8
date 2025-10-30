5bb3529ada7873fd3dfb95b4d5c7b7773f48f042
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-30 20:24:15 +0900

    feat(constant): 상수 파일 분리

--- src/constant.js
+++ src/constant.js
@@ -2,3 +2,20 @@ export const REQUIRED_NUMBERS = 6;
 export const FORBIDDEN_INPUT = /[^ ,0-9일이삼사오육칠팔구십백천만원]/;
 export const MAXIMUM_AMMOUNT = 100000;
 export const MINIMUM_AMMOUNT = 1000;
+export const NUMBER_MAP = {
+  일: 1,
+  이: 2,
+  삼: 3,
+  사: 4,
+  오: 5,
+  육: 6,
+  칠: 7,
+  팔: 8,
+  구: 9,
+};
+
+export const DIGIT_NUMBER_MAP = {
+  십: 2,
+  백: 1,
+  천: 0,
+};
